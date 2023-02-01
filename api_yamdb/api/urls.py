c1037de1c28eaa5af3b5689c6fdaf4008c533c95
AlenaLezzhova <43500999+AlenaLezzhova@users.noreply.github.com>
2023-02-01 22:10:01 +0300

    Update urls.py

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,10 +1,9 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import (CategoryViewSet, CommentViewSet,
-                    GenreViewSet, get_jwt_token,
-                    ReviewViewSet, send_confirmation_code,
-                    TitleViewSet, UserViewSet)
+from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
+                    ReviewViewSet, TitleViewSet, UserViewSet, get_jwt_token,
+                    send_confirmation_code)
 
 app_name = 'api'
 v1 = routers.DefaultRouter()
