d1fd4437b9234a910356858755bd1817629f8bea
AlenaLezzhova <43500999+AlenaLezzhova@users.noreply.github.com>
2023-02-01 21:23:21 +0300

    Update views.py

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -6,20 +6,21 @@ from rest_framework import filters, status, viewsets
 from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from api_yamdb.settings import DEFAULT_FROM_EMAIL
 from reviews.models import Category, Genre, Review, Title, User
+
 from .filters import TitleFilter
 from .mixins import ListCreateDestroyViewSet
-from .permissions import (IsAdminOrReadOnly, IsAdmin,
+from .permissions import (IsAdmin, IsAdminOrReadOnly,
                           IsAuthorOrAdminOrModerator)
 from .serializers import (CategorySerializer, CommentSerializer,
+                          CheckConfirmationCodeSerializer,
                           GenreSerializer, ReviewSerializer,
-                          TitleCreateSerializer, TitleListSerializer,
-                          UserSerializer, SendCodeSerializer,
-                          CheckConfirmationCodeSerializer)
+                          SendCodeSerializer, TitleCreateSerializer,
+                          TitleListSerializer, UserSerializer)
 
 
 @api_view(['POST'])
